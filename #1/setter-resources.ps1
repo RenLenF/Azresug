ecf98657e3bc066d8ae85d1818c2cf964b4c0c25
Ren Fujii <fujii.ren@fixer.co.jp>
2019-07-24 20:29:20 +0900

    修正2点
    ①テスト用ファイルとディレクトリを削除
    ②#1のファイルを追加

---
+++ #1/setter-resources.ps1
@@ -0,0 +1,35 @@
+﻿#
+# setter-resources.ps1
+#
+$appName = $ProjectName + "-setter"
+$serverName = $ProjectName + "-plan"
+$keyVaultName = $ProjectName + "-keyvault"
+$Today = Get-Date -Format "yyyyMMdd"
+$storageName = $ProjectName + $Today + "sa"
+
+$rgLocation = (Get-AzResourceGroup -Name $rgName).Location
+$serverId = (Get-AzResource -ResourceGroupName $rgName -ResourceType microsoft.web/serverfarms -Name $serverName).ResourceId
+
+
+New-AzResource -ResourceGroupName $rgName -Location $rgLocation -ResourceType Microsoft.Storage/storageAccounts -ResourceName $storageName `
+               -kind "StorageV2" -Sku @{name="Standard_LRS";tier=0} -Properties @{AccessTier="Hot"} -Force
+$storageKey = Get-AzStorageAccountKey -ResourceGroupName $rgName -AccountName $storageName
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=" + $storageName + ";AccountKey=" + $storageKey + "2019-04-01"
+$prop=@{serverFarmId=$serverId;`
+        siteConfig=@{`
+            "nodeVersion"="10.14.1";`
+            "linuxFxVersion"="DOCKER|mcr.microsoft.com/azure-functions/node:2.0-node8-appservice";
+            "appSettings"=@(@{name="AzureWebJobsStorage";value=$connectionString };`
+                            @{name="WEBSITE_RUN_FROM_PACKAGE";value=$zipUrl};`
+                            @{name="FUNCTIONS_WORKER_RUNTIME";value="node" };`
+                            @{name="WEBSITE_NODE_DEFAULT_VERSION";value="10.14.1"};`
+                            @{name="FUNCTIONS_EXTENSION_VERSION";value="2"};`
+                            @{name="VAULT_NAME";value=$keyVaultName}`
+                          );`
+            "cors"=@{allowedOrigins=@("*")};`
+            "reserved"="true"`
+        };`
+        identity=@{type="SystemAssigned"}}
+
+New-AzResource -ResourceGroupName $rgName -Location $rgLocation -ResourceType Microsoft.Web/sites `
+               -ResourceName $appName -kind "functionapp,linux,container"  -Properties $prop -Force
---
+++ #1/shared-resources.ps1
@@ -0,0 +1,24 @@
+﻿#
+# shared-resources.ps1
+#
+$serverName = $ProjectName + "-plan"
+$keyVaultName = $ProjectName + "-keyvault"
+$rgLocation = (Get-AzResourceGroup -Name $rgName).Location
+New-AzResource -ResourceGroupName $rgName -Location $rgLocation -ResourceType microsoft.web/serverfarms -ResourceName $serverName `
+               -kind linux -Properties @{reserved="true"} -Sku @{name="B1";tier="Basic"; size="B1"; family="B"; capacity="1"} -Force
+
+New-AzKeyVault -VaultName $keyVaultName -ResourceGroupName $rgName -Location $rgLocation
+Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ResourceGroupName $rgName `
+                           -ObjectId $MyId -PermissionsToSecrets get,list,set,delete,backup,restore,recover `
+                           -PermissionsToKeys decrypt,encrypt,unwrapkey,wrapkey,verify,sign,get,list,update,create,import,delete,backup,restore,recover
+
+$secureString = ConvertTo-SecureString -string "default" -AsPlainText -Force
+Set-AzKeyVaultSecret -VaultName $keyVaultName -Name "default" -SecretValue $secureString
+
+$cnt = 0
+for($idx=0; $idx -lt 2; $idx++) {
+    $name = "secretName" + $idx
+    $secureString = ConvertTo-SecureString -string $name -AsPlainText -Force
+    Set-AzKeyVaultSecret -VaultName $keyVaultName -Name $name -SecretValue $secureString
+}
+
